ebd6cb79e978f659d870425eddc6cfedb01b30e4
Matthew Smithson <mtthwsmthsn@gmail.com>
2020-03-13 16:08:01 -0700

    Update Multiplication.cs

--- Multiplication.cs
+++ Multiplication.cs
@@ -6,6 +6,7 @@ class Program
 	static int score {get;set;}
 	static int status {get;set;}
 	static int highscore {get;set;}
+	static int answer {get;set;}
 	static void tooslow(object source, ElapsedEventArgs e) 
 	{ 
 		status = 0;
@@ -21,8 +22,16 @@ class Program
 			Console.WriteLine($"Congrats, you have set a new high score!");
 		}
 	}
+	static void read_answer()
+	{
+		int x;
+		int.TryParse(Console.ReadLine(),out x);
+		answer = x;
+		if(answer==0) {read_answer();};
+	}
 	static void Main()
 	{
+		Console.Clear();
 		highscore = int.Parse(File.ReadAllText("highscore.txt"));
 		Console.WriteLine($"Current Highscore of {highscore}");
 		Console.WriteLine("You have 3 seconds to answer");
@@ -37,16 +46,13 @@ class Program
 			int a = r.Next(1,10);
 			int b = r.Next(1,10);
 			Console.WriteLine($"{a} * {b} ?");
-			int x = int.Parse(Console.ReadLine());
-			Console.WriteLine(" ");
-			if(x == (a*b))
+			read_answer();
+			if(answer == (a*b))
 			{	
 				score++;
-				Console.WriteLine($"Good job, new score of {score}");
 			}
 			else
 			{
-
 				Console.WriteLine($"Wrong, correct answer is {a*b}");
 				status = 0;
 				for(int i = 1; i<=10; i++) {Console.WriteLine($"{a} * {i} = {a*i}");};
